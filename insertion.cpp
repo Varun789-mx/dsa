b43169d9236af8d59623e050d9f65a14e94c454d
varun789-mx <varunsh153@gmail.com>
2025-04-11 19:27:49 +0000

    Fix the insertion sort

--- insertion.cpp
+++ insertion.cpp
@@ -5,19 +5,14 @@ using namespace std;
 int main() {
   int arr[] = {6, 5, 4, 3, 2, 1};
   int len = sizeof(arr) / sizeof(arr[0]);
-  for (int i = 0; i < len; i++) {
-    for (int j = i; j < len; j++) {
-      if (arr[i] > arr[j]) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
-      if (arr[i] < arr[j]) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-      }
+  for (int i = 1; i < len; i++) {
+    int temp = arr[i];
+    int j = i - 1;
+    while (j >= 0 && temp < arr[j]) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = temp;
   }
 
   for (int i = 0; i < len; i++) {
