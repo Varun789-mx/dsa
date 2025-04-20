48710ee1db3f92ea04d70f8e615b87c2775d4b68
varun789-mx <varunsh153@gmail.com>
2025-04-20 18:10:16 +0000

    add class base solution

--- arrays/sortchecker.cpp
+++ arrays/sortchecker.cpp
@@ -3,22 +3,25 @@
 
 using namespace std;
 
-bool Checker(vector<int> &arr) {
-  bool flag = true;
-  for (int i = 0; i < arr.size() - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      flag = false;
-      return flag;
+class SortChecker {
+public:
+  bool Checker(vector<int> &arr) {
+    bool flag = true;
+    for (int i = 0; i < arr.size() - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        flag = false;
+        return flag;
+      }
     }
+    return flag;
   }
-  return flag;
-}
-
+};
 int main() {
   vector<int> nums = {1, 2, 3, 4, 5};
   vector<int> arr = {1, 4, 6, 2, 6, 2, 6};
+  SortChecker res;
 
-  if (!Checker(nums)) {
+  if (!res.Checker(arr)) {
     cout << "False" << endl;
   } else {
     cout << "True" << endl;
